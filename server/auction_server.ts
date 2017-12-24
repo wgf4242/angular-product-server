839e97778f7cd7849ba0dd7696792418ad4df7e2
wgf4242 <wgf4242@gmail.com>
2017-12-24 22:41:05 +0800

    update

--- server/auction_server.ts
+++ server/auction_server.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as path from "path";
 import { Server } from "ws";
 
 const app = express();
@@ -8,7 +9,9 @@ export class Product {
     public id: number,
     public title: string,
     public price: number,
-    public rating: number
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
   ) {}
 }
 
@@ -24,14 +27,14 @@ export class Comment {
 }
 
 const products: Product[] = [
-  new Product(1, "第一个商品", 1.99, 3.5),
-  new Product(2, "第二个商品", 2.99, 2.5),
-  new Product(3, "第三个商品", 3.99, 4.5),
-  new Product(4, "第四个商品", 4.99, 1.5),
-  new Product(5, "第五个商品", 5.99, 3.5),
-  new Product(6, "第六个商品", 6.99, 2.5)
+  new Product(1, "第一个商品", 1.99, 3.5, "这是第一个商品,是我在学习", [ "电子产品"]),
+  new Product(2, "第二个商品", 2.99, 2.5, "这是第二个商品,是我在学习", [ "图书" ]),
+  new Product(3, "第三个商品", 3.99, 4.5, "这是第三个商品,是我在学习", [ "硬件设备" ]),
+  new Product(4, "第四个商品", 4.99, 1.5, "这是第四个商品,是我在学习", [ "电子产品", "硬件设备"]),
+  new Product(5, "第五个商品", 5.99, 3.5, "这是第五个商品,是我在学习", [ "电子产品" ]),
+  new Product(6, "第六个商品", 6.99, 2.5, "这是第六个商品,是我在学习", ["图书", "硬件设备" ])
 ];
-
+  
 const comments: Comment[] = [
   new Comment(1, 1, "2017-02-02 22:22:22", "张三", 3, "东西不错"),
   new Comment(2, 1, "2017-03-03 23:22:22", "李四", 4, "东西是不错"),
@@ -39,38 +42,79 @@ const comments: Comment[] = [
   new Comment(4, 2, "2017-05-05 20:22:22", "赵六", 4, "东西还不错")
 ];
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+//这时还不能刷新,  Angular 要用 localhost:8000/ 下找路由,在 provider 中添加路由策略
+app.use("/", express.static(path.join(__dirname, '..', 'client')))
+
+// app.get("/", (req, res) => { res.send("Hello Express"); });
+
+app.get("/api/products", (req, res) => {
+  let result = products;
+  let params = req.query;
+
+  if (params.title) {
+    result = result.filter(p => p.title.indexOf(params.title) !== -1);
+  }
+
+  if (params.price && result.length > 0) {
+    result = result.filter(p => p.price <= parseInt(params.price));
+    
+  }
+
+  if (params.category && params.category !== "-1" && result.length > 0) {
+    result = result.filter(p => p.categories.indexOf(params.category) !== -1);
+    console.log(params, "cate", params.category);
+  }
+  console.log(result);
+  
+  res.json(result);
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   res.json(products.find(product => product.id == req.params.id));
 });
 
-app.get('/api/products/:id/comments', (req, res) => {
-  res.json(comments.filter((comment: Comment) => comment.productId == req.params.id));
+app.get("/api/products/:id/comments", (req, res) => {
+  res.json(
+    comments.filter((comment: Comment) => comment.productId == req.params.id)
+  );
 });
 
 const server = app.listen(8000, "localhost", () => {
   console.log("服务器已启动, 地址是: http://localhost:8000");
 });
 
+const subsciption = new Map<any, number[]>();
+
 const wsServer = new Server({ port: 8085 });
 wsServer.on("connection", websocket => {
-  websocket.send("这个消息是服务器主动推送的");
+  // websocket.send("这个消息是服务器主动推送的");
   websocket.on("message", message => {
-    console.log("接收到消息" + message);
+    let messageObj = JSON.parse(message.toString());
+    let productIds = subsciption.get(websocket) || [];
+    subsciption.set(websocket, [...productIds, messageObj.productId]);
   });
 });
 
+const currentBids = new Map<number, number>();
+
+
 setInterval(() => {
-  if (wsServer.clients) {
-    wsServer.clients.forEach(client => {
-      client.send("这是定时推送");
-    });
-  }
+  products.forEach( p => {
+    let currentBid = currentBids.get(p.id) || p.price;
+    let newBid = currentBid + Math.random()*5;
+    currentBids.set(p.id, newBid);
+    console.log(newBid);
+  });
+
+  subsciption.forEach((productIds: number[], ws) => {
+    if (ws.readyState === 1) {
+      let newBids = productIds.map(pid => ({
+        productId: pid,
+        bid: currentBids.get(pid)
+      }));
+      ws.send(JSON.stringify(newBids));
+    } else {
+      subsciption.delete(ws);
+    }
+  })
 }, 2000);
